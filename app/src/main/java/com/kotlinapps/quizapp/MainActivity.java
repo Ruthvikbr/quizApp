23dd7f21ae1d9263c9caaaea4e006c7f01739f2d
Ruthvikbr <ruthvikbr24@gmail.com>
2020-05-02 14:26:59 +0530

    add list and settings

--- app/src/main/java/com/kotlinapps/quizapp/MainActivity.java
+++ app/src/main/java/com/kotlinapps/quizapp/MainActivity.java
@@ -1,7 +1,14 @@
 package com.kotlinapps.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.list_activity :
+                Intent intent = new Intent(MainActivity.this,ListActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.settings_fragment :
+                Log.i("mainActivity","Settings");
+                return true;
+            default:  return super.onOptionsItemSelected(item);
+        }
+
     }
 }
