9335db4139d945108b849f7398cab177cc7c7b75
Ruthvikbr <ruthvikbr24@gmail.com>
2020-05-07 20:52:52 +0530

    add state-json and prepopulate DB

--- app/src/main/java/com/kotlinapps/quizapp/database/StateDatabase.java
+++ app/src/main/java/com/kotlinapps/quizapp/database/StateDatabase.java
@@ -1,8 +1,7 @@
 package com.kotlinapps.quizapp.database;
 
-import android.app.Application;
 import android.content.Context;
-import android.util.Log;
+import android.content.res.AssetManager;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -12,6 +11,13 @@ import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.kotlinapps.quizapp.data.State;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -32,11 +38,71 @@ public abstract class StateDatabase  extends RoomDatabase {
                            context.getApplicationContext()
                            ,StateDatabase.class
                            ,"State"
-                    ).fallbackToDestructiveMigration().build();
+                    ).addCallback(new Callback() {
+                        @Override
+                        public void onCreate(@NonNull final SupportSQLiteDatabase db) {
+                            super.onCreate(db);
+                            executor.execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                    populateFromDB(context.getAssets(),INSTANCE.stateDao());
+
+                                }
+                            });
+                        }
+                    }).fallbackToDestructiveMigration().build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    private static void populateFromDB(AssetManager assetManager,StateDao stateDao) {
+        BufferedReader bufferedReader = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        String json = "";
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(assetManager.open("sate-capital.json")));
+            String mLine;
+            while ((mLine = bufferedReader.readLine()) != null) {
+                stringBuilder.append(mLine);
+            }
+            json = stringBuilder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+        }
+        try{
+            JSONObject states = new JSONObject(json);
+            JSONObject section = states.getJSONObject("sections");
+            populateFromJSON(section.getJSONArray("States (A-L)"), stateDao);
+            populateFromJSON(section.getJSONArray("States (M-Z)"), stateDao);
+            populateFromJSON(section.getJSONArray("Union Territories"), stateDao);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void populateFromJSON(JSONArray states,StateDao stateDao){
+        try{
+            for(int i =0;i<states.length();i++){
+                JSONObject stateData = states.getJSONObject(i);
+                String stateName = stateData.getString("key");
+                String stateCapital = stateData.getString("val");
+                stateDao.InsertState(new State(stateName,stateCapital));
+            }
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
