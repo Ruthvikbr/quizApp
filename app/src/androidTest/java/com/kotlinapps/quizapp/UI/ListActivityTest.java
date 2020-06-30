0da20242d659f287fd0829b523246f29cdfed12b
Ruthvikbr <ruthvikbr24@gmail.com>
2020-06-30 17:57:54 +0530

    add test

---
+++ app/src/androidTest/java/com/kotlinapps/quizapp/UI/ListActivityTest.java
@@ -0,0 +1,32 @@
+package com.kotlinapps.quizapp.UI;
+
+
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+
+import com.kotlinapps.quizapp.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
+
+@RunWith(AndroidJUnit4.class)
+public class ListActivityTest {
+
+    @Rule
+    public IntentsTestRule<ListActivity> activityTestRule = new IntentsTestRule<>(ListActivity.class);
+
+    @Test
+    public void activityLaunch(){
+        onView(withId(R.id.addState)).perform(click());
+        onView(withId(R.id.titleTV)).check(matches(isDisplayed()));
+    }
+}
